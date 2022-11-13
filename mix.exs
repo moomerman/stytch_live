a06cc104a0085f5fe6d8e58f3ee3fd0673795796
Richard Taylor <moomerman@gmail.com>
2022-11-13 15:27:49 +0000

    Add stytch API client

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule StytchLive.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "0.29.0", only: :dev, runtime: false}
+      {:ex_doc, "0.29.0", only: :dev, runtime: false},
+      {:stytch, "~> 0.1.0"}
     ]
   end
 end
